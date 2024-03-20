ae4b1d8e110dfb813521baf9b212679ab7d7acb9
katiejnete <156715616+katiejnete@users.noreply.github.com>
2024-03-19 18:16:46 -0700

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -6,9 +6,6 @@ from models import db, User, Post
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
-app.config['TESTING'] = True
-
-app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 db.drop_all()
 db.create_all()
